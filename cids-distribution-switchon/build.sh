ed477180171b234fa5df166ecdc9d15cb1a474ae
p-a-s-c-a-l <pascal@cismet.de>
2016-12-06 19:48:37 +0100

    #9 jarsigning issues (partly) resolved

--- cids-distribution-switchon/build.sh
+++ cids-distribution-switchon/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -t switchon/cids-distribution-switchon:latest --no-cache=true .
+# brute force invalidate complete cache
+#docker build -t switchon/cids-distribution-switchon:latest --no-cache=true .
+
+# set build timestamp to selectively invalidate cache after dockerfile instruction
+docker build -t switchon/cids-distribution-switchon:latest --build-arg expire_after=$(date +%Y-%m-%d:%H:%M:%S) .
