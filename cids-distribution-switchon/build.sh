c08c892200eba240f72c3ab256d66bba674301a4
p-a-s-c-a-l <pascal@cismet.de>
2017-06-01 10:53:54 +0200

    add --squash build flag to remove .private data from image history

--- cids-distribution-switchon/build.sh
+++ cids-distribution-switchon/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# add --squash build flag once it is available to remove .private data from image history
+# add --squash build flag available to remove .private data from image history
 # See https://github.com/docker/docker/pull/22641#issuecomment-266156133
 
 # brute force invalidate complete cache
@@ -8,7 +8,7 @@
 
 # set build timestamp to selectively invalidate cache after dockerfile instruction
 # RELEASE BUILD
-#docker build -t switchon/cids-distribution-switchon:latest -t switchon/cids-distribution-switchon:5.0 --build-arg expire_after=$(date +%Y-%m-%d:%H:%M:%S) .
+#docker build --squash -t switchon/cids-distribution-switchon:latest -t switchon/cids-distribution-switchon:5.0 --build-arg expire_after=$(date +%Y-%m-%d:%H:%M:%S) .
 
 # DEV BUILD
-docker build -t switchon/cids-distribution-switchon:5.0-SNAPSHOT -t switchon/cids-distribution-switchon:latest-snapshot --build-arg expire_after=$(date +%Y-%m-%d:%H:%M:%S) .
+docker build --squash -t switchon/cids-distribution-switchon:5.0-SNAPSHOT -t switchon/cids-distribution-switchon:latest-snapshot --build-arg expire_after=$(date +%Y-%m-%d:%H:%M:%S) .
